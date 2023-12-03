fb655aa52f85950dbb57d8b179ed08f324f8fa39
y-takagiiii <takagiyuri0712@gmail.com>
2023-12-03 19:24:32 +0900

    ユーザー登録apiを作成

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 const app = express();
 const PORT = 8000;
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+const prisma = new PrismaClient();
+// jsonを使う宣言
+app.use(express.json());
+// 新規ユーザー登録
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const saltRound = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRound);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return res.json({ user });
 });
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
